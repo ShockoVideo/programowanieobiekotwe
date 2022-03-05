849ec4dbe0f4cce3ee2e1d2b541ef21b17e15c8e
ShockoVideo <p.michal@live.com>
2022-03-05 17:04:51 +0100

    Próba

---
+++ App1/App1/Fraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    class Fraction
+    {
+
+        private int Nominator;
+        private int Denominator;
+   
+    }
+
+
+}
